69970989e543efbc7d864cb5ea1bc71c8624f550
Aleh Belanouski <aleh_belanouski@epam.com>
2022-12-05 11:34:16 +0300

    Task nodejs-basics

--- src/fs/utils.mjs
+++ src/fs/utils.mjs
@@ -17,28 +17,3 @@ export const log = (text, value) => value ? console.log(text, value) : console.l
 
 export const __filenameGet = meta => fileURLToPath(meta);
 export const __dirnameGet = meta => path.dirname(__filenameGet(meta));
-
-// module.exports = {
-//   fileExists,
-//   logError,
-//   log,
-// }
-// const {access} = require("fs/promises");
-// const {constants} = require("fs");
-// const fileExists = async (name) => {
-//   try {
-//     await access(name, constants.F_OK);
-//     return true;
-//   } catch (e) {
-//     return false;
-//   }
-// }
-//
-// const logError = (text, value) => value ? console.error(text, value) : console.error(text);
-// const log = (text, value) => value ? console.log(text, value) : console.log(text);
-//
-// module.exports = {
-//   fileExists,
-//   logError,
-//   log,
-// }
